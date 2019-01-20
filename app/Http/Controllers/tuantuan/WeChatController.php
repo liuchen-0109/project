9ce0ab45744d0e394a969f5333b643e176d05697
liuchen <2354921286@qq.com>
2019-01-20 15:14:21 +0800

    esaywechat

--- app/Http/Controllers/tuantuan/WeChatController.php
+++ app/Http/Controllers/tuantuan/WeChatController.php
@@ -13,15 +13,12 @@ class WeChatController extends Controller
 
     function __construct()
     {
-        $config = [
-            'app_id' => 'wxd4a50070d4f44a26',
-            'secret' => 'a5d16852f75c1852c73c75bd061dff56',
-            'token' => 'liuchen',
-            'response_type' => 'array',
-            //...
-        ];
 
-        $this->app = Factory::officialAccount($config);
+        $this->app = Factory::officialAccount(Config('project.tuantuan'));
+//        $oauth = $this->app->oauth;
+//        if (empty($_SESSION['wechat_user'])) {
+//            return $oauth->redirect();
+//        }
     }
 
     /**
---
+++ config/project.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'tuantuan' => ['app_id' => 'wxd4a50070d4f44a26',
+        'secret' => 'a5d16852f75c1852c73c75bd061dff56',
+        'token' => 'liuchen',
+        'response_type' => 'array',
+        'oauth' => [
+            'scopes' => ['snsapi_userinfo'],
+            'callback' => '/oauth_callback',
+        ],
+
+    ],
+];
